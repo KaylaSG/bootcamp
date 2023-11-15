4aceecfc203f9884beeb9206208a7373af3f4cfc
KaylaSG <93552203+KaylaSG@users.noreply.github.com>
2023-11-15 10:11:34 -0500

    Finish logical operations

--- section16/Logical_Operators/app.js
+++ section16/Logical_Operators/app.js
@@ -6,3 +6,46 @@ if(password.length >=6 && password.indexOf(' ') === -1){
     console.log("INCORRECT PASSWORD FORMAT")
 }
 
+//OR(||) ONE MUST BE TRUE
+
+const age = 90;
+if(age >= 0 && age < 5 || age >= 65){
+    console.log("Free")
+}
+else if(age >= 5 && age < 10){
+    console.log('$10')
+}
+else if (age >= 10 && age < 65){
+    console.log('$20')
+}else{
+    console.log('invalid age')
+}
+
+//NOT(!)
+const firstName = prompt('Enter your first name');
+if(!firstName){
+    firstName = prompt('Please enter your first name')
+}
+
+
+//SWITCH statement
+const day = 2;
+switch (day{
+    case 1:
+        console.log('Monday');
+        break;
+    case 2:
+        console.log('Tuesday');
+        break;
+    case 3:
+        console.log('Wednesday');
+        break;
+    case 4:
+        console.log('Thursdsay');
+        break;
+    case 5:
+        console.log('Friday');
+        break;
+    default:
+        console.log('I dont know that');
+})
