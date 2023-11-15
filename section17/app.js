29f8dce4297c0c1da4ea8b840b317fd49c13ca0f
KaylaSG <93552203+KaylaSG@users.noreply.github.com>
2023-11-15 10:16:40 -0500

    Start arrays

---
+++ section17/app.js
@@ -0,0 +1,13 @@
+//Arrays are lists
+
+//Makes an empty array
+let colors = [];
+
+//Array of strings
+let colors=['purple', 'pink', 'blue', 'green']
+
+//Array of numbers
+let lottoNums = [19, 22, 56, 12, 51]
+
+//Mixed array
+let stuff = [true, 68, 'cat', null]
